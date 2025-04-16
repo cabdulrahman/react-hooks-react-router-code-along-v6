77d222d86beb1a6cc22fb8cedb97cfda5e7f73ab
abdulrahman <abdulrahmanibnimohammed@gmail.com>
2025-04-16 10:24:22 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// Step 1. Import react-router functions
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    }
+  ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Home />);
